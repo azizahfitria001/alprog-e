ab59e4d56babddbef9b9726b033aa34cd639561f
azizahfitria001 <azizahfitria001@gmail.com>
2024-11-08 08:51:00 +0700

    Add files via upload

---
+++ prog3.py
@@ -0,0 +1,25 @@
+# variabel assigment value 
+x = 24         
+Y = 74
+print("nilai dari x adalah : ", y)
+
+# aturan penamaa variabel
+luas = 60 # bisa berupa kata 
+edy_ganteng = 100 # jika dua kata, digabung dg "_"    
+RaraX = 21 boleh gabungkan capital non-kapital
+RaraX = 27
+lencana88 = 20 # jika menggabungkan huruf dan angka, angkanya di akhir
+print("tipe dari kezia adalah : ",)
+
+
+a = 6 # integer bilangan bulet 
+print("tipe dari a : ", type (a))
+
+b = 9,8 # float bilangan pecahan 
+print("tipe data dari b : ", type (b))
+
+c = "keren" #  string, gabungan karakter 
+print("tipe data dari c : ", )
+
+d = False # Boolean, true false
+print("tipe data dari d : ", type (d))
