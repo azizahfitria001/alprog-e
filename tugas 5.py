e45b0a3f9fabeea5040e0a6a9bd1462397921080
azizahfitria001 <azizahfitria001@gmail.com>
2024-11-11 21:14:24 +0800

    Add files via upload

---
+++ tugas 5.py
@@ -0,0 +1,10 @@
+print("nim = 24241174")
+print("nimAwal = 24")
+print("nimAkhir = 74")
+
+
+print("panjang = 74")
+print("lebar = 24")
+
+print("hasil = 98 * panjang * lebar")
+print("hasil dari luas x {panjang} x {tinggi} adalah {hasil} =")
