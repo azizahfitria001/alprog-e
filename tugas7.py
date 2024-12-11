e9aacb3df902d4e018e8e2c9cf6df132f4888e1d
azizahfitria001 <azizahfitria001@gmail.com>
2024-12-12 07:35:05 +0800

    Add files via upload

---
+++ tugas7.py
@@ -0,0 +1,9 @@
+# Menghitung luas persegi 
+# Nama : Azizah Fitriah
+
+print("menghitung luas persegi")
+panjang = int(input("masukkan panjang : "))
+lebar = int(input("masukkan lebar : "))
+
+hasil = panjang * lebar 
+print(f"Hasil dari {panjang} x {lebar} = {hasil}")
